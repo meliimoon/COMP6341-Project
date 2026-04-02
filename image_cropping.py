267e2d3885d57a100479873272e09e4dc42648ae
Melissa Ananian <118542902+meliimoon@users.noreply.github.com>
2026-04-02 14:37:44 -0400

    Added cropping for train dataset
    **Still hardcoded to my PC's file paths**
    Changed the script to crop the train images as well. Cropped license plates get saved to separate folders for the train and validation sets.

--- image_cropping.py
+++ image_cropping.py
@@ -2,50 +2,63 @@ import os
 import cv2
 from ultralytics import YOLO
 
+# Perform license plate detection and cropping using our best trained YOLOv8 model, and save the cropped images to specified directories for both training and validation sets.
+
 # Load in the trained model 
 # File path created automatically by the training process of YOLOv8
 model = YOLO(r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\runs\detect\license_plate_model\weights\best.pt")
 
-input_directory = r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\dataset\images\val"
-output_directory = r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\cropped_plates"
+input_directory_val = r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\dataset\images\val"
+input_directory_train = r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\dataset\images\train"
 
-os.makedirs(output_directory, exist_ok=True)
+output_directory_val = r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\cropped_plates\val"
+output_directory_train = r"C:\Users\Meliimoon\Documents\Concordia\Grad\Winter 2026\COMP6341\project\ProjectCode\cropped_plates\train"
 
-# Loop through images in the input dataset directory, perform detection, and save cropped license plate images
-for filename in os.listdir(input_directory):
-    if filename.endswith((".jpg", ".png")):
-        image_path = os.path.join(input_directory, filename)
-        img = cv2.imread(image_path)
+os.makedirs(output_directory_val, exist_ok=True)
+os.makedirs(output_directory_train, exist_ok=True)
 
-        results = model(image_path)
-        # Or can do:
-        #    results = model(image_path, conf=0.5) # directly sets confidence threshold for detection, so only detections with conf >= 0.5 will be returned
-        # Then can remove the confidence check in the loop below
-        #    for i, box in enumerate(results[0].boxes.xyxy):
-        # Then remove the if statement
+directory_IOs = [
+    (input_directory_val, output_directory_val),
+    (input_directory_train, output_directory_train)
+]
 
-        # Loop through detected bounding boxes and crop the license plate regions
-        for i, (box, conf) in enumerate(zip(results[0].boxes.xyxy, results[0].boxes.conf)):
+# Loop through images in the train and validation set directories, perform detection, and save cropped license plate images
+for input_directory, output_directory in directory_IOs:
+    for filename in os.listdir(input_directory):
+        if filename.endswith((".jpg", ".png")):
+            image_path = os.path.join(input_directory, filename)
+            img = cv2.imread(image_path)
 
-            # Only save cropped images for detections with high confidence scores
-            if conf < 0.5:
-                continue
+            results = model(image_path)
+            
+            # (Side note) Alternatively can do:
+            #    results = model(image_path, conf=0.5) # directly sets confidence threshold for detection, so only detections with conf >= 0.5 will be returned
+            # Then can remove the confidence check in the loop below
+            #    for i, box in enumerate(results[0].boxes.xyxy):
+            # Then remove the if statement
 
-            x_min, y_min, x_max, y_max = map(int, box.tolist())
+            # Loop through detected bounding boxes and crop the license plate regions
+            for i, (box, conf) in enumerate(zip(results[0].boxes.xyxy, results[0].boxes.conf)):
 
-            # Pad the bounding box in case the detected box is too tight around the license plate
-            h, w, _ = img.shape
-            pad = 10
+                # Only save cropped images for detections with high confidence scores
+                if conf < 0.5:
+                    continue
 
-            x_min = max(0, x_min - pad)
-            y_min = max(0, y_min - pad)
-            x_max = min(w, x_max + pad)
-            y_max = min(h, y_max + pad)
+                x_min, y_min, x_max, y_max = map(int, box.tolist())
 
-            # Crop the image using the bounding box coordinates
-            cropped = img[y_min:y_max, x_min:x_max]
+                # Pad the bounding box in case the detected box is too tight around the license plate
+                h, w, _ = img.shape
+                pad = 10
 
-            save_path = os.path.join(output_directory, f"{filename}_plate_{i}.jpg")
-            cv2.imwrite(save_path, cropped)
+                x_min = max(0, x_min - pad)
+                y_min = max(0, y_min - pad)
+                x_max = min(w, x_max + pad)
+                y_max = min(h, y_max + pad)
 
-            print(f"Saved: {save_path}")
+                # Crop the image using the bounding box coordinates
+                cropped = img[y_min:y_max, x_min:x_max]
+
+                save_path = os.path.join(output_directory, f"{filename}_plate_{i}.jpg")
+                cv2.imwrite(save_path, cropped)
+
+                print(f"Saved: {save_path}")
